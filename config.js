edd0d1bdaf711b8ba280007ec57aa8f512712bd5
vladlukhanin <vladlukhanin@gmail.com>
2017-06-29 16:54:49 +0300

    Beta 0.0.1

--- config.js
+++ config.js
@@ -1,23 +1,28 @@
 export default {
     taskRepeatTime: 30, // minutes
+
     stackoverflow: {
         key: '*1PeMRfvryE0zEHbx)AbKg((',
         pagesize: 100 // max count is 100
     },
+
     quickblox: {
         appId: 36125,
         authKey: 'gOGVNO4L9cBwkPE',
         authSecret: 'JdqsMHCjHVYkVxV',
+
         config: {
             debug: {
                 mode: 0
             }
         },
+
         bot: {
             id: 27285539,
             login: 'stackoverflow_bot',
             password: 'qb_stackoverflow_bot'
         },
+
         dataClassName: 'StackOverflowBot'
     }
 };
